d337b6b13ad2f95c0bb504e136ad056a8e91a97f
“st10123” <stakenobu@icloud.com>
2024-02-18 12:34:29 +0900

    changed style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,11 +3,12 @@ import { View, Text, StyleSheet } from 'react-native'
 const Index = (): JSX.Element => {
   return (
       <View style={styles.container}>
+
         {/* 画面上の帯のところの設定 */}
-        <View>
-            <View>
-              <Text>Memo App</Text>
-              <Text>ログアウト</Text>
+        <View style={styles.header}>
+            <View style={styles.headerInner}>
+              <Text style={styles.headerTitle}>Memo App</Text>
+              <Text style={styles.headerRight}>ログアウト</Text>
             </View>
         </View>
         {/* 画面下のメモ一覧の設定 */}
@@ -59,8 +60,28 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
   }
 })
 
