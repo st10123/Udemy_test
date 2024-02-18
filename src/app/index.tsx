9a712868c7fdee9e98b5b74b959433dcd9e98240
“st10123” <stakenobu@icloud.com>
2024-02-18 11:11:52 +0900

    Create View

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,67 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-      <View>
-        <Text>Hello World</Text>
+      <View style={styles.container}>
+        {/* 画面上の帯のところの設定 */}
+        <View>
+            <View>
+              <Text>Memo App</Text>
+              <Text>ログアウト</Text>
+            </View>
+        </View>
+        {/* 画面下のメモ一覧の設定 */}
+        <View>
+
+            <View>
+                {/* 左側のメモの名前 */}
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2024年2月</Text>
+                </View>
+                {/* 右側のバツボタン */}
+                <View>
+                    <Text>X</Text>
+                </View>
+            </View>
+
+            <View>
+                {/* 左側のメモの名前 */}
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2024年2月</Text>
+                </View>
+                {/* 右側のバツボタン */}
+                <View>
+                    <Text>X</Text>
+                </View>
+            </View>
+
+            <View>
+                {/* 左側のメモの名前 */}
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2024年2月</Text>
+                </View>
+                {/* 右側のバツボタン */}
+                <View>
+                    <Text>X</Text>
+                </View>
+            </View>
+        </View>
+        <View>
+            <Text>+</Text>
+        </View>
       </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
