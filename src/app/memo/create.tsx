8dfd37cb60b8e53b393ddcb1b7e603c9b667f94a
“st10123” <stakenobu@icloud.com>
2024-02-24 23:24:25 +0900

    add firestore

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -3,20 +3,43 @@ import {
 } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { router } from 'expo-router'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
 
 import CircleButton from '../../components/CircleButton'
+import { db, auth } from '../../config'
 
-const handlepress = (): void => {
+const handlepress = (bodyText: string): void => {
+//   メモ作成のコード
+  if (auth.currentUser === null) { return }
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+  addDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
+  })
+    .then(() => {
+      console.log('success')
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   router.back()
 }
 
 const Create = (): JSX.Element => {
+  const [bodyText, setBodyText] = useState('')
   return (
     <KeyboardAvoidingView behavior='height' style={styles.container}>
         <View style={styles.inputContainer}>
-            <TextInput multiline style={styles.input} value= '' />
+            <TextInput
+            multiline
+            style={styles.input}
+            value= {bodyText}
+            onChangeText={(text) => { setBodyText(text) }}
+            autoFocus
+            />
         </View>
-        <CircleButton onPress={handlepress}>
+        <CircleButton onPress={() => { handlepress(bodyText) }}>
             <Feather name='plus' size={40}/>
         </CircleButton>
     </KeyboardAvoidingView>
