2c50015bc040c0940132fee46fe7aef39e800b64
“st10123” <stakenobu@icloud.com>
2024-02-24 15:05:34 +0900

    add reaction of button

--- src/app/auth/Login.tsx
+++ src/app/auth/Login.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 
 import Header from '../../components/Header'
 import Button from '../../components/Button'
@@ -14,7 +14,9 @@ const Login = (): JSX.Element => {
             <Button label='Submit' />
             <View style={styles.footer}>
                 <Text style={styles.footerText}>Not registered?</Text>
-                <Text style={styles.footerLink}>Sign up here!</Text>
+                <TouchableOpacity>
+                 <Text style={styles.footerLink}>Sign up here!</Text>
+                </TouchableOpacity>
             </View>
         </View>
     </View>
