7fa3303b83d5afd458fff843f49daace92ecd543
“st10123” <stakenobu@icloud.com>
2024-02-24 17:54:58 +0900

    create logout button

--- src/app/auth/Login.tsx
+++ src/app/auth/Login.tsx
@@ -6,7 +6,7 @@ import { Link, router } from 'expo-router'
 
 const hundlePress = (): void => {
   // ログイン
-  router.push('memo/list')
+  router.replace('memo/list')
 }
 
 const Login = (): JSX.Element => {
--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,15 +1,24 @@
 import { View, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
-import { router } from 'expo-router'
+import { router, useNavigation } from 'expo-router'
+import { useEffect } from 'react'
 
 import MemoList from '../../components/MemoList'
 import CircleButton from '../../components/CircleButton'
+import Logoutbutton from '../../components/LogOutButton'
 
 const handlepress = (): void => {
   router.push('/memo/create')
 }
 
 const List = (): JSX.Element => {
+  const navigation = useNavigation()
+  // useEffectは一回画面がレンダリングされた時に実行される
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => { return <Logoutbutton /> }
+    })
+  }, [])
   return (
       <View style={styles.container}>
         {/* 画面下のメモ一覧の設定 */}
